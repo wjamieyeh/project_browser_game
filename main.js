139b6a6e314281f578c9127d9b2eabbfb83f2255
Jamie Yeh <jamie.yeh@disney.com>
2018-04-16 14:52:43 -0700

    working start screen on click

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+$(function () {
 
+  console.log('page is ready');
+
+  $('#start').click(function () {
+    $(this).css('z-index', '0');
+  });
+
+  $('#start').hover(function () {
+    $(this).css('color', 'rgb(252, 131, 0, 1)');
+  }, function () {
+    $(this).css('color', '');
+  });
+
+
+});
