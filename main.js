af5497ad9fd0ab26c9d309fb6e14d8d5ae8acfa5
Jamie Yeh <jamie.yeh@disney.com>
2018-04-17 14:35:07 -0700

    adds multiple birds

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 $(function () {
 
-  console.log('page is ready');
+  //console.log('page is ready');
 
   $('#start').click(function () {
     $(this).css('z-index', '0');
+    startGame();
   });
 
   $('#start').hover(function () {
@@ -11,6 +12,35 @@ $(function () {
   }, function () {
     $(this).css('color', '');
   });
+
+  //have the flying object show up and start flying
+  // function startGame() {
+  //   console.log('startGame is working');
+  //
+  //   //create an img tag with flying bird object and insert after clicking start.
+  //   let $bird1 = $('<img>', {class: 'bird01', src: 'img/birdFlying1.gif'});
+  //   $('#start').after($bird1);
+  //   $bird1.css({"position": "relative", "z-index": "1", "bottom": "300px","left": "400px", "width": "50px", "height": "50px", "cursor": "url('https://cdn4.iconfinder.com/data/icons/miscellaneous-icons-3/200/pointer_cross_aim-32.png'), auto"});
+  //
+  // }
+
+  for (let i = 0; i < 5; i++) {
+      let $bird = $('<img>', {src: 'img/birdFlying1.gif', width: '50', height: '50'});
+      $('#start').after($bird);
+      $bird.css({"position": "relative", "z-index": "1", "bottom": "300px","left": "400px", "cursor": "url('https://cdn4.iconfinder.com/data/icons/miscellaneous-icons-3/200/pointer_cross_aim-32.png'), auto"});
+  }
+
+
+
+
+  // function createBird () {
+  //   console.log('createBird is working');
+  //   let
+  // }
+  //add event listener to click
+
+
+
 
 
 });
